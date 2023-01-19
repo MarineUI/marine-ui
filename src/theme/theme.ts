beb532c67f6984201ea11756b0e05f4a909f9999
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-20 00:14:33 +0800

    feat: add more color

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -8,6 +8,14 @@ export default {
       light: '#EADDFF',
       dark: '#4F378B',
     },
+    onPrimary: {
+      light: '#FFFFFF',
+      dark: '#371E73',
+    },
+    onPrimaryContainer: {
+      light: '#21005E',
+      dark: '#EADDFF',
+    },
     secondary: {
       light: '#625B71',
       dark: '#CCC2DC',
@@ -16,6 +24,14 @@ export default {
       light: '#E8DEF8',
       dark: '#4A4458',
     },
+    onSecondary: {
+      light: '#FFFFFF',
+      dark: '#332D41',
+    },
+    onSecondaryContainer: {
+      light: '#1E192B',
+      dark: '#E8DEF8',
+    },
     tertiary: {
       light: '#7D5260',
       dark: '#EFB8C8',
@@ -23,6 +39,14 @@ export default {
     tertiaryContainer: {
       light: '#FFD8E4',
       dark: '#633B48',
+    },
+    onTertiary: {
+      light: '#FFFFFF',
+      dark: '#492532',
+    },
+    onTertiaryContainer: {
+      light: '#370B1E',
+      dark: '#FFD8E4',
     },
   },
 };
