c9495e26d32f73b77cc21f151ffc484542cb75ce
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-20 00:33:30 +0800

    feat: add all colors

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -48,5 +48,77 @@ export default {
       light: '#370B1E',
       dark: '#FFD8E4',
     },
+    surface: {
+      light: '#FFFBFE',
+      dark: '#1C1B1F',
+    },
+    surfaceVariant: {
+      light: '#E7E0EC',
+      dark: '#49454F',
+    },
+    onSurface: {
+      light: '#1C1B1F',
+      dark: '#E6E1E5',
+    },
+    onSurfaceVariant: {
+      light: '#49454E',
+      dark: '#CAC4D0',
+    },
+    background: {
+      light: '#FFFBFE',
+      dark: '#1C1B1F',
+    },
+    onBackground: {
+      light: '#1C1B1F',
+      dark: '#E6E1E5',
+    },
+    error: {
+      light: '#B3261E',
+      dark: '#F2B8B5',
+    },
+    errorContainer: {
+      light: '#F9DEDC',
+      dark: '#8C1D18',
+    },
+    onError: {
+      light: '#FFFFFF',
+      dark: '#601410',
+    },
+    onErrorContainer: {
+      light: '#410E0B',
+      dark: '#F9DEDC',
+    },
+    outline: {
+      light: '#79747E',
+      dark: '#938F99',
+    },
+    outlineVariant: {
+      light: '#C4C7C5',
+      dark: '#444746',
+    },
+    shadow: {
+      light: '#000000	',
+      dark: '#000000	',
+    },
+    surfaceTint: {
+      light: '#6750A4',
+      dark: '#D0BCFF',
+    },
+    inverseSurface: {
+      light: '#313033',
+      dark: '#E6E1E5',
+    },
+    inverseOnSurface: {
+      light: '#F4EFF4',
+      dark: '#313033',
+    },
+    inversePrimary: {
+      light: ' #D0BCFF',
+      dark: ' #6750A4',
+    },
+    scrim: {
+      light: '#000000',
+      dark: '#000000',
+    },
   },
 };
