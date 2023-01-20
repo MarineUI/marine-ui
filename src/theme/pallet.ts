57efcd4871f3cdbff0de400b7d9f124cb2d815a5
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-20 21:09:00 +0800

    refactor: move constant out of function

--- src/theme/pallet.ts
+++ src/theme/pallet.ts
@@ -5,17 +5,18 @@ import {
 } from '@material/material-color-utilities';
 import themeColor from './theme';
 
+const tones = [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99];
+const keyColors = [
+  'primary40',
+  'secondary40',
+  'tertiary40',
+  'error40',
+  'neutral40',
+  'neutralVariant40',
+];
+
 // 通过几个颜色生成整个色谱
 function generatePallet(themeColors: any) {
-  const tones = [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99];
-  const keyColors = [
-    'primary40',
-    'secondary40',
-    'tertiary40',
-    'error40',
-    'neutral40',
-    'neutralVariant40',
-  ];
   const pallet: any = {};
   keyColors.forEach((key) => {
     pallet[key] = {};
