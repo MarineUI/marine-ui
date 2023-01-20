76ea5d6abbf3a1b86dd3bc3a753b128e38bd4dfa
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-20 21:01:43 +0800

    feat: generate pallet from theme

---
+++ src/theme/pallet.ts
@@ -0,0 +1,32 @@
+import {
+  argbFromHex,
+  hexFromArgb,
+  TonalPalette,
+} from '@material/material-color-utilities';
+import themeColor from './theme';
+
+// 通过几个颜色生成整个色谱
+function generatePallet(themeColors: any) {
+  const tones = [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99];
+  const keyColors = [
+    'primary40',
+    'secondary40',
+    'tertiary40',
+    'error40',
+    'neutral40',
+    'neutralVariant40',
+  ];
+  const pallet: any = {};
+  keyColors.forEach((key) => {
+    pallet[key] = {};
+    tones.forEach((tone) => {
+      pallet[key][tone] = hexFromArgb(
+        TonalPalette.fromInt(argbFromHex(themeColors[key])).tone(tone)
+      );
+    });
+  });
+  return pallet;
+}
+
+const pallet = generatePallet(themeColor);
+export default pallet;
