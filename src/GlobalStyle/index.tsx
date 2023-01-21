3cf9eacefb5f3ecd95309a934d59b2e256298982
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-21 15:36:04 +0800

    feat: globale font style

---
+++ src/GlobalStyle/index.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+  font-family: 'Roboto', sans-serif;
+
+  margin: 0;
+  padding: 0;
+`;
+
+export default GlobalStyle;
