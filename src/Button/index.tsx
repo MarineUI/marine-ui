f318260db9cf2ec625c5f9e5ef4e02345cd2277a
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-22 22:38:13 +0800

    feat: add global style

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GlobalStyle from '../GlobalStyle';
 import Typography from '../Typography';
 import { ButtonProps } from './interface';
 import { ButtonStyle } from './style';
@@ -6,9 +7,12 @@ import { ButtonStyle } from './style';
 const MyButton = (props: ButtonProps) => {
   const { children } = props;
   return (
-    <ButtonStyle props={props} {...props}>
-      <Typography variant="label-large">{children as string}</Typography>
-    </ButtonStyle>
+    <>
+      <GlobalStyle />
+      <ButtonStyle props={props} {...props}>
+        <Typography variant="label-large">{children as string}</Typography>
+      </ButtonStyle>
+    </>
   );
 };
 
