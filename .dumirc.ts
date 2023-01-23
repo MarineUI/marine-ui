90ae948b5cd1cfb251e3f6045879a7e26774c715
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-23 22:06:38 +0800

    feat: move font import from css to html

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,4 +5,12 @@ export default defineConfig({
   themeConfig: {
     name: 'marine-ui',
   },
+  links: [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap',
+    },
+  ],
 });
--- src/GlobalStyle/index.tsx
+++ src/GlobalStyle/index.tsx
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
   font-family: 'Roboto', sans-serif;
 
   margin: 0;
