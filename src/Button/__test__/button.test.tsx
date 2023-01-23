5b0cc189fbc1034bbf0718c251dee6968711159a
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-23 21:39:55 +0800

    feat: button test

--- src/Button/__test__/button.test.tsx
+++ src/Button/__test__/button.test.tsx
@@ -1,12 +1,40 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import React from 'react';
 import MyButton from '..';
 
 describe('Button', () => {
-  test('Render the Button', () => {
+  test('Render default Button', () => {
     render(<MyButton>Hello World</MyButton>);
     expect(screen.getByRole('button')).toBeInTheDocument();
     expect(screen.getByRole('button')).toHaveTextContent('Hello World');
+    expect(screen.getByRole('button')).toBeEnabled();
+    cleanup();
+  });
+
+  test('Render Enabled Button', () => {
+    const variant = ['filled', 'outlined', 'text'];
+    variant.forEach((v) => {
+      render(<MyButton variant={v as any}>Hello World</MyButton>);
+      expect(screen.getByRole('button')).toBeInTheDocument();
+      expect(screen.getByRole('button')).toHaveTextContent('Hello World');
+      expect(screen.getByRole('button')).toBeEnabled();
+      cleanup();
+    });
+  });
+
+  test('Render Disabled Button', () => {
+    const variant = ['filled', 'outlined', 'text'];
+    variant.forEach((v) => {
+      render(
+        <MyButton variant={v as any} disabled>
+          Hello World
+        </MyButton>
+      );
+      expect(screen.getByRole('button')).toBeInTheDocument();
+      expect(screen.getByRole('button')).toHaveTextContent('Hello World');
+      expect(screen.getByRole('button')).toBeDisabled();
+      cleanup();
+    });
   });
 });
