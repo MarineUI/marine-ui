6d5693d1502f4547e0ae93dbae32eeecc39ba2e3
KelvinQiu <kelvinqiu802@outlook.com>
2023-01-28 19:07:42 +0800

    feat: button size

--- src/Button/interface.ts
+++ src/Button/interface.ts
@@ -4,6 +4,7 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color?: 'primary' | 'secondary' | 'tertiary';
   variant?: 'text' | 'filled' | 'outlined';
+  size?: 'small' | 'medium' | 'large';
 }
 
 export type Theme = 'light' | 'dark';
