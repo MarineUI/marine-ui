2f167be7b051cd30084d744b818a0b3701090791
YuZzzzzzzze <10857440+yuzzzzzzzze@user.noreply.gitee.com>
2023-02-09 22:10:32 +0800

    feat: add basic Radio

---
+++ src/Radio/interface.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+export interface RadioProps {
+  children: ReactNode;
+  className?: string;
+  disabled?: boolean;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as MyButton } from './Button';
+export { default as Radio } from './Radio';
 export { default as Space } from './Space';
 export { default as Switch } from './Switch';
